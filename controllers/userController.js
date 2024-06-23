dfea47f5c023db39ef0f60ccc6af493e8b8aceb5
Mortza81 <mortezaahmadinya.81@gmail.com>
2024-06-23 16:43:33 +0330

    fixing userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const appError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const handlerFactory = require("./handlerFactory");
 exports.getAllusers = handlerFactory.getAll(User);
@@ -7,25 +8,33 @@ exports.updateUser = handlerFactory.updateOne(User);
 exports.deleteUser = handlerFactory.deleteOne(User);
 exports.updateMe = catchAsync(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirm) {
-    return next(new appError('This route is not for changing password', 400))
+    return next(new appError("This route is not for changing password", 400));
   }
-  const objFiltered = filterObj(req.body, 'email', 'name')
-  objFiltered.photo = req.file.filename
+  const objFiltered = filterObj(req.body, "email", "name");
   const user = await User.findByIdAndUpdate(req.user.id, objFiltered, {
     runValidators: true,
     new: true,
-  })
+  });
   res.status(200).json({
-    status: 'success',
+    status: "success",
     user,
-  })
+  });
 });
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, {
     isActive: "false",
   });
   res.status(204).json({
-    status:'success',
-    data:'null'
-  })
+    status: "success",
+    data: "null",
+  });
 });
+const filterObj = (obj, ...allowedFields) => {
+  let newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
