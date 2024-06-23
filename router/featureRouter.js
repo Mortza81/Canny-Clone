2bf2d40a2274362287874c2f41d389e78c8c3727
Mortza81 <mortezaahmadinya.81@gmail.com>
2024-06-23 17:22:32 +0330

    protecting some routes

--- router/featureRouter.js
+++ router/featureRouter.js
@@ -1,10 +1,20 @@
 const featureController = require("../controllers/featureController");
 const express = require("express");
+const authController = require("../controllers/authController");
 const router = express.Router();
-router.route("/").post(featureController.create).get(featureController.getAll);
+router
+  .route("/")
+  .post(
+    authController.protect,
+    authController.restrict("user"),
+    featureController.create
+  )
+  .get(
+    featureController.getAll
+  );
 router
   .route("/:id")
   .get(featureController.getOne)
-  .delete(featureController.delete)
-  .patch(featureController.update);
+  .delete(authController.protect,featureController.delete)
+  .patch(authController.protect,featureController.update);
 module.exports = router;
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,14 +2,14 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 const authController = require("../controllers/authController");
-router.get("/", authController.protect, userController.getAllusers);
+router.get("/", authController.protect,authController.restrict('admin'), userController.getAllusers);
 router.patch("/updateMe", authController.protect, userController.updateMe);
 router.delete("/deleteMe", authController.protect, userController.deleteMe);
 router
   .route("/:id")
-  .get(userController.getOneuser)
-  .delete(userController.deleteUser)
-  .patch(userController.updateUser);
+  .get(authController.protect,authController.restrict('admin'),userController.getOneuser)
+  .delete(authController.protect,authController.restrict('admin'),userController.deleteUser)
+  .patch(authController.protect,authController.restrict('admin'),userController.updateUser);
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/forgotPassword", authController.forgotPassword);
