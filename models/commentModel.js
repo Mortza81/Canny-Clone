2f12d405782072474d05fae17196c3655170aa2a
Mortza81 <mortezaahmadinya.81@gmail.com>
2024-06-23 23:35:26 +0330

    adding populate options to comment and request models

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,23 +1,34 @@
-const mongoose=require('mongoose')
-const commentSchema=new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.ObjectId,
-        ref:'User'
+const mongoose = require("mongoose");
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
     },
-    request:{
-        type:mongoose.Schema.ObjectId,
-        ref:'Request'
-    }
-    ,
-    body:{
-        type:String,
-        required:[true,'Your comment should have a body text']
-    }
-    ,createdAt:{
-        type:Date,
-        default:Date.now()
-    }
+    request: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Request",
+    },
+    body: {
+      type: String,
+      required: [true, "Your comment should have a body text"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+commentSchema.index({ request: 1, user: 1 }, { unique: true });
+commentSchema.pre(/^find/,function(next){
+    this.populate({
+        path:'user',
+        select:'name'
+    })
 })
-reviewSchema.index({ request: 1, user: 1 }, { unique: true })
-const Comment=mongoose.model('Comment',commentSchema)
-module.exports=Comment
+const Comment = mongoose.model("Comment", commentSchema);
+module.exports = Comment;
--- models/requestModel.js
+++ models/requestModel.js
@@ -1,29 +1,46 @@
 const mongoose = require("mongoose");
-const requestSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-    maxlength: [20, "Title must have less or equal then 20 characters"],
-  },
-  category: String,
-  description: {
-    type: String,
-    required: [true, "Text is required"],
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: [true, "It must belongs to a user"],
-  },
-  images: [
-    {
+const requestSchema = new mongoose.Schema(
+  {
+    title: {
       type: String,
+      required: [true, "Title is required"],
+      maxlength: [20, "Title must have less or equal then 20 characters"],
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
+    category: String,
+    description: {
+      type: String,
+      required: [true, "Text is required"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "It must belongs to a user"],
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+requestSchema.virtual("reviews", {
+  ref: "Comment",
+  foreignField: "request",
+  localField: "_id",
+});
+requestSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "name",
+  });
 });
 const Request = mongoose.model("Request", requestSchema);
 module.exports = Request;
