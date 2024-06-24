d026165354c110fed0636024858c791cee6dce68
Mortza81 <mortezaahmadinya.81@gmail.com>
2024-06-24 15:57:08 +0330

    adding some additional options to request

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -7,10 +7,7 @@ exports.create = handlerFactory.createOne(Request);
 exports.getAll = handlerFactory.getAll(Request);
 exports.update = handlerFactory.updateOne(Request);
 exports.delete = handlerFactory.deleteOne(Request);
-exports.getOne = handlerFactory.getOne(Request, {
-  path: "comments",
-  select: ['body','user'],
-});
+exports.getOne = handlerFactory.getOne(Request);
 const multerStorage = multer.memoryStorage();
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.split("/")[0].startsWith("image")) {
@@ -26,12 +23,10 @@ const upload = multer({
 exports.uploadRequestImages = upload.fields([{ name: "images", maxCount: 2 }]);
 exports.resizeRequestImages = catchAsync(async (req, res, next) => {
   if (!req.files.images) return next();
-  req.body.images = [];
+  req.body.images=[]
   await Promise.all(
     req.files.images.map(async (file, index) => {
-      const filename = `request-${req.body.title}-${Date.now()}-${
-        index + 1
-      }.jpeg`;
+      const filename = `request-${req.body.title}-${Date.now()}-${index + 1}.jpeg`;
       await sharp(file.buffer)
         .resize(2000, 1333)
         .toFormat("jpeg")
--- models/requestModel.js
+++ models/requestModel.js
@@ -35,11 +35,6 @@ const requestSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
-requestSchema.virtual('comments', {
-  ref: 'Comment',
-  foreignField: 'request',
-  localField: '_id',
-})
 requestSchema.virtual("reviews", {
   ref: "Comment",
   foreignField: "request",
