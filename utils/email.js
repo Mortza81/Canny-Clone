4a0111f38da50761f48274a5bab213effadbde5b
Mortza81 <mortezaahmadinya.81@gmail.com>
2024-06-24 16:30:16 +0330

    implementing emails for development

--- utils/email.js
+++ utils/email.js
@@ -1,19 +1,21 @@
 const nodemailer = require('nodemailer')
+const dotenv=require('dotenv')
+dotenv.config()
 module.exports=class Email{
   constructor(user){
     this.to=user.email
     this.firstName=user.name.split(' ')[0]
-    this.from=`Moretza Ahmadi <${process.env.EMAIL_FROM}>`
+    this.from=`info@mortezadomain.ir`
   }
   newTransport(){
     if(process.env.NODE_ENV=='production'){
       return nodemailer.createTransport({
-        host: MAIL_HOST,
-        port: MAIL_PORT,
-        tls: true,
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        tls:true,
         auth: {
-          user: MAIL_USER,
-          pass: MAIL_PASSWORD,
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
         }
       });
     }
